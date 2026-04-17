00fc19a08365c52845bb98c97ed066f92c96879c
ziggyedman <michael.vaz@gmail.com>
2026-04-17 03:32:25 +0100

    commit 9c8b1e5f8a2d3e4f5g6h7i8j9k0l1m2n3o4p5q6

--- .next/types/validator.ts
+++ .next/types/validator.ts
@@ -74,6 +74,15 @@ type RouteHandlerConfig<Route extends AppRouteHandlerRoutes = AppRouteHandlerRou
   type __Unused = __Check
 }
 
+// Validate ../../app/api/source/route.ts
+{
+  type __IsExpected<Specific extends RouteHandlerConfig<"/api/source">> = Specific
+  const handler = {} as typeof import("../../app/api/source/route.js")
+  type __Check = __IsExpected<typeof handler>
+  // @ts-ignore
+  type __Unused = __Check
+}
+
 
 
 
